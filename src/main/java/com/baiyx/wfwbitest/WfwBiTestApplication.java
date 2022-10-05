b24fe0bbc4bcc4f79b2c657fa599b233b31c21bd
baiyx <784724280@qq.com>
2022-10-05 11:16:03 +0800

    1. 新增文件上传功能及其配置,并支持多文件上传.

--- src/main/java/com/baiyx/wfwbitest/WfwBiTestApplication.java
+++ src/main/java/com/baiyx/wfwbitest/WfwBiTestApplication.java
@@ -2,17 +2,25 @@ package com.baiyx.wfwbitest;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 //@EnableRabbit
 @EnableCaching
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class WfwBiTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WfwBiTestApplication.class, args);
+        // 启动打开默认浏览器访问
+        try {
+            Runtime.getRuntime().exec("cmd /c start http://localhost:9090/api/upload.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
