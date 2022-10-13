2c1cd0d9e0800850525c26718e4bd2b4773d6ea1
baiyx <784724280@qq.com>
2022-10-13 16:59:27 +0800

    1. 完善测试接口TokenAccessController:测试BI大屏传参Token,解析token之后,去数据库比对查询信息,返回权限状态

--- src/main/java/com/baiyx/wfwbitest/controller/TokenAccessController.java
+++ src/main/java/com/baiyx/wfwbitest/controller/TokenAccessController.java
@@ -5,14 +5,11 @@ import com.baiyx.wfwbitest.customAnnotations.WebLog;
 import com.baiyx.wfwbitest.entity.TokenAccess;
 import com.baiyx.wfwbitest.service.TokenAccessService;
 import com.baiyx.wfwbitest.util.ResolveTokenUtil;
-import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
 
 /**
  * @Author: 白宇鑫
@@ -27,11 +24,14 @@ public class TokenAccessController {
     @Autowired
     TokenAccessService tokenAccessService;
 
-    @WebLog(description = "查询所有")
-    @RequestMapping(value = "check",method= RequestMethod.POST,produces = "application/json")
-    public int check(@RequestBody String token){
-        JSONObject jsonObject = JSONObject.parseObject(token);
-        TokenAccess tokenAccess =  ResolveTokenUtil.resolveToken(jsonObject.getString("token"));
-        return tokenAccessService.checkStatus(tokenAccess);
+    @WebLog(description = "权限检查")
+    @RequestMapping(value = "check",method= {RequestMethod.GET,RequestMethod.POST},produces = "application/json")
+    @CrossOrigin
+    public JSONObject check(HttpServletRequest request){
+        JSONObject data = new JSONObject();
+        TokenAccess tokenAccess =  ResolveTokenUtil.resolveToken(request.getHeader("x-gisq-token"));
+        int status = tokenAccessService.checkStatus(tokenAccess);
+        data.put("status",status);
+        return data;
     }
 }
