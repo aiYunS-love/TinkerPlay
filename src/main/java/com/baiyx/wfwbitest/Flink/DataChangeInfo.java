3da4b7661977f7ded5b41d40fc25c944055c9e93
baiyx <784724280@qq.com>
2023-04-27 20:41:32 +0800

    1. 整合flink

---
+++ src/main/java/com/baiyx/wfwbitest/Flink/DataChangeInfo.java
@@ -0,0 +1,49 @@
+package com.baiyx.wfwbitest.Flink;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author: 白宇鑫
+ * @Date: 2023年4月27日, 0027 下午 6:52:59
+ * @Description: CDC 数据实体类
+ */
+@Data
+public class DataChangeInfo {
+
+
+    /**
+     * 变更前数据
+     */
+    private String beforeData;
+    /**
+     * 变更后数据
+     */
+    private String afterData;
+    /**
+     * 变更类型 1新增 2修改 3删除
+     */
+    private Integer eventType;
+    /**
+     * binlog文件名
+     */
+    private String fileName;
+    /**
+     * binlog当前读取点位
+     */
+    private Integer filePos;
+    /**
+     * 数据库名
+     */
+    private String database;
+    /**
+     * 表名
+     */
+    private String tableName;
+    /**
+     * 变更时间
+     */
+    private Long changeTime;
+
+}
