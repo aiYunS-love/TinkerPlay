ad0ec97d0af9420292f92ba659c3353da4f4d0bd
baiyx <784724280@qq.com>
2023-05-02 11:33:21 +0800

    1. 整合ChatGPT

--- src/main/java/com/baiyx/wfwbitest/Controller/AiController.java
+++ src/main/java/com/baiyx/wfwbitest/Controller/AiController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.net.Proxy;
