99a6f75de3e010939beadbf2fc702ce20acac07a
baiyx <784724280@qq.com>
2023-05-07 09:33:11 +0800

    1. 收集了一些算法

---
+++ src/main/java/com/baiyx/wfwbitest/Algorithm/LongestCommonSubsequence.java
@@ -0,0 +1,34 @@
+package com.baiyx.wfwbitest.Algorithm;
+
+/**
+ * @Author: 白宇鑫
+ * @Date: 2023年5月7日, 0007 上午 8:49:54
+ * @Description: 动态规划来解决最长公共子序列问题
+ * 最长公共子序列（Longest Common Subsequence，LCS）是指在两个字符串中找到最长的子序列（可以不连续），使得它在两个原始字符串中都出现
+ */
+
+public class LongestCommonSubsequence {
+
+    public static int longestCommonSubsequence(String str1, String str2) {
+        int m = str1.length(), n = str2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcde";
+        String str2 = "ace";
+        System.out.println(longestCommonSubsequence(str1, str2));
+    }
+}
---
+++ src/main/java/com/baiyx/wfwbitest/Algorithm/RecursiveDP.java
@@ -0,0 +1,22 @@
+package com.baiyx.wfwbitest.Algorithm;
+
+/**
+ * @Author: 白宇鑫
+ * @Date: 2023年5月7日, 0007 上午 9:00:37
+ * @Description: 递归来解决动态规划问题: 计算斐波那契数列的第6项
+ */
+
+public class RecursiveDP {
+
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println(fib(n));
+    }
+}
