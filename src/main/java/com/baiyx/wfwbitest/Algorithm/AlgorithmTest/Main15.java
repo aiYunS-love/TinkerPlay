9a78be49db7108b985d9e3b781269aa6c3a4e8ce
baiyx <784724280@qq.com>
2023-05-10 18:37:52 +0800

    1. 千里马常有,而伯乐不常有!

---
+++ src/main/java/com/baiyx/wfwbitest/Algorithm/AlgorithmTest/Main15.java
@@ -0,0 +1,33 @@
+package com.baiyx.wfwbitest.Algorithm.AlgorithmTest;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * @Author: 白宇鑫
+ * @Date: 2023年5月10日, 0010 下午 5:13:14
+ * @Description: 最长公共前缀
+ */
+
+public class Main15 {
+    public static String a(String[] strs){
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args){
+        String[] a = {"flower","flow","flight"};
+        System.out.println(a(a));
+    }
+}
