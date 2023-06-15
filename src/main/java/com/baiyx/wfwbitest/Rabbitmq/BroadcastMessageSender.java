e1623124f9fcc14d55ad0602291d1f39549e4668
baiyx <784724280@qq.com>
2023-06-15 14:36:13 +0800

    MQ异步广播消息

--- src/main/java/com/baiyx/wfwbitest/Rabbitmq/BroadcastMessageSender.java
+++ src/main/java/com/baiyx/wfwbitest/Rabbitmq/BroadcastMessageSender.java
@@ -1,8 +1,5 @@
 package com.baiyx.wfwbitest.Rabbitmq;
 
-import org.springframework.amqp.AmqpException;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,15 +16,7 @@ public class BroadcastMessageSender {
     private RabbitTemplate rabbitTemplate;
 
     public void sendBroadcastMessage(String message, final long delayTimes) {
-        rabbitTemplate.convertAndSend("exchange_broadcast", "", message, new MessagePostProcessor() {
-            @Override
-            public Message postProcessMessage(Message message) throws AmqpException {
-                //给广播设置延迟毫秒值
-                message.getMessageProperties().setExpiration(String.valueOf(delayTimes));
-                System.out.println("设置延迟时间完成!");
-                return message;
-            }
-        });
+        rabbitTemplate.convertAndSend("exchange_broadcast", "", message);
         System.out.println("广播消息发送完成!");
     }
 }
