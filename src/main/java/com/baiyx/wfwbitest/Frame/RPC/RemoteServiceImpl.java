90868753c9facdd229cfd777c589993fc42a2a41
baiyx <784724280@qq.com>
2023-08-29 17:09:43 +0800

    全面升级框架,模拟RPC通讯

---
+++ src/main/java/com/baiyx/wfwbitest/Frame/RPC/RemoteServiceImpl.java
@@ -0,0 +1,13 @@
+package com.baiyx.wfwbitest.Frame.RPC;
+
+/**
+ * @Author: baiyx
+ * @Date: 2023年8月29日, 0029 下午 4:58:04
+ * @Description: 实现服务接口的具体实现类
+ */
+public class RemoteServiceImpl implements RemoteService {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
