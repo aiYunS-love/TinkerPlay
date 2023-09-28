4e924cfb7b203154e3aa3d61c30b9c0e5595ba19
bai784724280 <58358565+bai784724280@users.noreply.github.com>
2023-09-28 09:32:32 +0800

    工厂模式 demo

---
+++ src/main/java/com/baiyx/wfwbitest/Other/DesignMode/Rectangle.java
@@ -0,0 +1,13 @@
+package com.baiyx.wfwbitest.Other.DesignMode;
+
+/**
+ * @Author: baiyx
+ * @Date: 2023年9月28日, 0028 上午 9:23:44
+ * @Description: 工厂模式: 定义图形接口的实现类矩形类
+ */
+public class Rectangle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("画出了一个矩形!");
+    }
+}
