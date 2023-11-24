5d8e8f080897b24bf1b26c006d33238d70c2fa41
bai784724280 <58358565+bai784724280@users.noreply.github.com>
2023-11-24 11:13:49 +0800

    算法 字符串相似度

--- src/main/java/com/baiyx/tinkerplay/Algorithm/StringSimilarity.java
+++ src/main/java/com/baiyx/tinkerplay/Algorithm/StringSimilarity.java
@@ -9,42 +9,37 @@ import java.util.Arrays;
  */
 
 public class StringSimilarity {
-    public static void main(String[] args) {
-        String str1 = "hello";
-        String str2 = "holle";
-
-        int similarity = calculateSimilarity(str1, str2);
-        System.out.println("相似程度: " + similarity + "%");
-    }
-
-    public static int calculateSimilarity(String str1, String str2) {
-        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+    public static int calculateLevenshteinDistance(String str1, String str2) {
+        int[][] distance = new int[str1.length() + 1][str2.length() + 1];
 
         for (int i = 0; i <= str1.length(); i++) {
             for (int j = 0; j <= str2.length(); j++) {
                 if (i == 0) {
-                    dp[i][j] = j;
+                    distance[i][j] = j;
                 } else if (j == 0) {
-                    dp[i][j] = i;
+                    distance[i][j] = i;
                 } else {
-                    dp[i][j] = min(dp[i - 1][j - 1] + costOfSubstitution(str1.charAt(i - 1), str2.charAt(j - 1)),
-                            dp[i - 1][j] + 1,
-                            dp[i][j - 1] + 1);
+                    distance[i][j] = min(
+                            distance[i - 1][j - 1] + (str1.charAt(i - 1) == str2.charAt(j - 1) ? 0 : 1),
+                            distance[i - 1][j] + 1,
+                            distance[i][j - 1] + 1
+                    );
                 }
             }
         }
 
-        int maxLen = Math.max(str1.length(), str2.length());
-        int similarity = (int) (((double) (maxLen - dp[str1.length()][str2.length()]) / maxLen) * 100);
-        return similarity;
+        return distance[str1.length()][str2.length()];
     }
 
-    private static int costOfSubstitution(char a, char b) {
-        return a == b ? 0 : 1;
+    private static int min(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
     }
 
-    private static int min(int... numbers) {
-        return Arrays.stream(numbers)
-                .min().orElse(Integer.MAX_VALUE);
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "hola";
+        int distance = calculateLevenshteinDistance(str1, str2);
+
+        System.out.println("Levenshtein Distance: " + distance);
     }
 }
