b8c7da3ecca72ae1f22c6248a9b339e9c53f6b2a
bai784724280 <58358565+bai784724280@users.noreply.github.com>
2023-12-25 14:03:57 +0800

    配合拦截器,实现限流的基本思路和原理

--- src/main/java/com/baiyx/tinkerplay/Config/FilterConfig.java
+++ src/main/java/com/baiyx/tinkerplay/Config/FilterConfig.java
@@ -1,6 +1,6 @@
 package com.baiyx.tinkerplay.Config;
 
-import com.baiyx.tinkerplay.Other.InterceptorAndFilter.MyFilter;
+import com.baiyx.tinkerplay.InterceptorAndFilter.MyFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +16,7 @@ public class FilterConfig {
     public FilterRegistrationBean<MyFilter> myFilterRegistrationBean() {
         FilterRegistrationBean<MyFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new MyFilter());
-        registrationBean.addUrlPatterns("/api/RedisController/getAuthCode"); // 设置过滤器的匹配路径
+        registrationBean.addUrlPatterns("/*"); // 设置过滤器的匹配路径
         registrationBean.setOrder(1); // 设置过滤器的执行顺序，可选
         return registrationBean;
     }
